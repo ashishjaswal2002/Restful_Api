d8dc14863882ad0888e788883e205360d073e812
Ashish jaswal <jaswalashish131@gmail.com>
2023-02-17 00:00:27 +0530

    mongoose updated

--- app.js
+++ app.js
@@ -36,5 +36,43 @@ const articleSchema = {
     }
 };
 //Create a model in mongo
+
 const Article = mongoose.model('Article',articleSchema);
-//On video 366 the starting.....
+//WHen a client request the server that i want all the articles.. 
+app.get("/articles",function(req,res){
+    Article.find(function(err,foundArticle){
+        if(!err){
+            res.send(foundArticle);
+        }else{
+      res.send(err.message);
+        }
+    });
+})
+
+//Post request  to the server
+app.post('/articles', function (req, res) {
+    const newArticle = new Article({
+        title:req.body.title,
+        content:req.body.content      
+    }) ;
+
+    newArticle.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Successfully saved");
+        }
+    })
+    
+  });
+
+  app.delete('/articles',function(req,res){
+     Article.deleteMany(function(err){
+        if(err){
+            res.send("Error: " + err.message);
+        }else{
+           res.send("Successfully deleted");
+        }
+     })
+  })
+  
